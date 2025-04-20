2d08edf264615ddb2a0918123c04787f9e8103e4
zh <zh@DESKTOP-J6TK3B0>
2025-04-20 13:59:33 +0800

    更换动态加载 baseURL 实现方式

--- vuecli/src/request.js
+++ vuecli/src/request.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-let request;
-
-if (!request) {
-    await fetch('/config.json').then(res => res.json()).then(({ url }) => {
-        request = axios.create({
-            baseURL: url,
-            timeout: 1000,
-        });
-    });
-}
+const config = require("./config.json");
+const request = axios.create({
+    baseURL: config.url,
+    timeout: 1000,
+});
 export default request;
 
